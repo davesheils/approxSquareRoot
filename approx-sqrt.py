0068704bff07a6f7126225ae6c9150cbcf0e4951
davesheils <davesheils@gmail.com>
2019-10-12 15:22:15 +0100

    Second commit

---
+++ approx-sqrt.py
@@ -0,0 +1,15 @@
+def sqrt(x):
+    # adapted from https://tour.golang.org/flowcontrol/8
+    # initial guess
+    z = 1.0
+    # keep getting a better estimate for square 
+    # root of x until you are within 2 decimal places
+    
+    while abs(z*z - x) > 0.01:
+        # get better approx for sqrt
+        z -= (z*z - x) / (2*z)
+    return z
+
+
+# Calculate sqrt of 8
+sqrt(8.0)
